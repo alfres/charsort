db13b21d32d6cfac4c10be93c3728ab15f341db5
alfres <alfre_alfree@yahoo.es>
2017-03-29 23:14:46 +0200

    Update charsort_lista.js

--- charsort_lista.js
+++ charsort_lista.js
@@ -1,1 +1,1 @@
-var lista_1 = ["sopa:SE COME CON CUCHARA" , "diversión:MUCHA POLICIA, POCA ..." , "casco:VA EN MOTO" , "aprieta:QUIEN MUCHO ABARCA, POCO ..." , "roma:TODOS LOS CAMINOS CONDUCEN A ..."]
+var lista_1 = ["sopa:SE COME CON CUCHARA" , "diversión:MUCHA POLICIA, POCA ..." , "casco:VA EN MOTO" , "aprieta:QUIEN MUCHO ABARCA, POCO ..." , "roma:TODOS LOS CAMINOS CONDUCEN A ..." , "ojos:CRIA CUERVOS Y TE SACARAN LOS ...]
